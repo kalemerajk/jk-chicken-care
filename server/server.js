db06f6365918ac796cab92c0fba6ce7571730fec
kalemerajk <kalemerason2008@gmail.com>
2026-08-26 15:56:53 +0300

    test

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/api/auth', authRoutes);
 app.use('/api/stock-types', stockTypeRoutes);
 app.use('/api/orders', orderRoutes);
